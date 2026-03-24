3fbfef5503287a9dd7af409f807fd624afcb08ca
Vinicius Sarmento <sarmento.ramos11@gmail.com>
2026-03-23 21:38:39 -0300

    Struct GameState finalizada e implementada a logica inical de aplicação de dano

--- go-engine/internal/domain/boss/profiles.go
+++ go-engine/internal/domain/boss/profiles.go
@@ -4,7 +4,33 @@ import "sync"
 
 type GameState struct {
 	mu              sync.RWMutex
-	BossHP          int
+	BossHP          float64
 	Multiplicadores map[string]float64
 	ActiveIncident  string
+}
+
+func NewGameState(initialHP float64) *GameState {
+	return &GameState{
+		BossHP:          initialHP,
+		Multiplicadores: make(map[string]float64),
+		ActiveIncident:  "",
+	}
+}
+
+func (g *GameState) ApplyDamage(baseDamage float64) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	mult, set := g.Multiplicadores["damage"]
+
+	if !set {
+		mult = 1.0
+	}
+
+	currentDamage := baseDamage * mult
+	g.BossHP -= currentDamage
+
+	if g.BossHP < 0 {
+		g.BossHP = 0
+	}
 }
